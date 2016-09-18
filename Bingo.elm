4b64ddf99914f2c7e323e5553960eab7f78c4a2e
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-09-18 01:11:04 -0700

    add listing entries

--- Bingo.elm
+++ Bingo.elm
@@ -12,15 +12,32 @@ title message times =
     |> text
 
 pageHeader =
-  h1 [ id "logo", class "classy" ] [ title "Bingo!" 3 ] -- list of attributes, child element (Html type)
+  h1 [ ] [ title "Bingo!" 3 ]
+  -- list of attributes, child element (Html type)
 
 pageFooter =
   footer [ ]
-    [ a [ href "http://pragstudio.com" ]
-        [ text "The Pragmatic Studio" ]
+    [ a [ href "http://pragstudio.com" ] [ text "The Pragmatic Studio" ]
     ]
+
+entryItem phrase points =
+  li [ ]
+    [ span [ class "phrase" ] [ text phrase ]
+    , span [ class "points" ] [ text (toString points) ]
+    ]
+
+entryList =
+  ul [ ]
+    [ entryItem "Future-Proof" 100
+    , entryItem "Doing Agile" 200
+    ]
+
 view =
-  div [ id "container" ] [ pageHeader, pageFooter ]
+  div [ id "container" ]
+    [ pageHeader
+    , entryList
+    , pageFooter
+    ]
 
 
 main =
